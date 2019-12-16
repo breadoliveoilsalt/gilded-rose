e101e25eeb7ddd62ae668b69cbc64d947c914837
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-16 16:31:23 -0500

    Add test 1 for Aged Brie

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -45,16 +45,30 @@ describe GildedRose do
         expect(items[0].quality).to eq 4
       end
 
-      it "does not lower the quality below 0" do
+      xit "does not lower the quality below 0" do
         items = [
-          Item.new("Ordinary Item 1", 10, 0),
+          Item.new("Ordinary Item 1", 10, 0), 
           Item.new("Ordinary Item 2", -10, 0)
         ]
 
         GildedRose.new(items).update_quality()
         
         expect(items[0].quality).to eq 0
-        expect(items[0].quality).to eq 0
+        expect(items[1].quality).to eq 0
+      end
+
+      describe "Aged Brie" do
+
+        
+        it "increases the quality of Aged Brie if the quality is less than 50" do
+          items = [
+            Item.new("Aged Brie", 10, 45)
+          ]
+
+          GildedRose.new(items).update_quality()
+          
+          expect(items[0].quality).to eq 46
+        end
       end
 
     end
