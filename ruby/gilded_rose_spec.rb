1be72cc29ef89a0160c09c94d2f5ac7822ae2cdf
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-16 17:10:25 -0500

    Add test for sell_in date lowering in contexts to date

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -67,32 +67,50 @@ describe GildedRose do
         expect(items[0].quality).to eq 4
       end
 
-      context "for Aged Brie" do
+      it "lowers the sell_in date by 1" do
+        items = [Item.new("Ordinary Item", 10, 5)]
         
-        it "increases the quality of Aged Brie if the quality is less than 50" do
-          items = [
-            Item.new("Aged Brie", 10, 45)
-          ]
+        GildedRose.new(items).update_quality()
 
-          GildedRose.new(items).update_quality()
-          
-          expect(items[0].quality).to eq 46
-        end
-
+        expect(items[0].sell_in).to eq 9
       end
 
-      context "Sulfuras" do
+    end
 
-        it "its quality never changes" do
-          items = [
-            Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
-          ]
+    context "for Aged Brie" do
+      
+      it "increases the quality of Aged Brie if the quality is less than 50" do
+        items = [
+          Item.new("Aged Brie", 10, 45)
+        ]
 
-          GildedRose.new(items).update_quality()
-          
-          expect(items[0].quality).to eq 80
-        end
+        GildedRose.new(items).update_quality()
+        
+        expect(items[0].quality).to eq 46
+      end
+      
+      it "lowers the sell_in date by 1" do
+        items = [
+          Item.new("Aged Brie", 10, 45)
+        ]
 
+        GildedRose.new(items).update_quality()
+        
+        expect(items[0].sell_in).to eq 9
+      end
+
+    end
+
+    context "Sulfuras" do
+
+      it "its quality never changes" do
+        items = [
+          Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
+        ]
+
+        GildedRose.new(items).update_quality()
+        
+        expect(items[0].quality).to eq 80
       end
 
     end
