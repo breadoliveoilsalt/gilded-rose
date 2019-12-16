ce8bca4cb6dff4773d14114bedd1ae94116346ba
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-16 17:12:38 -0500

    Add sell_in test for Sulfuras

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -103,7 +103,7 @@ describe GildedRose do
 
     context "Sulfuras" do
 
-      it "its quality never changes" do
+      it "does not change the quality of Sulfuras" do
         items = [
           Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
         ]
@@ -113,6 +113,15 @@ describe GildedRose do
         expect(items[0].quality).to eq 80
       end
 
+      it "does not lower the sell_in date for Sulfuras" do
+        items = [
+          Item.new("Sulfuras, Hand of Ragnaros", 10, 80)
+        ]
+
+        GildedRose.new(items).update_quality()
+        
+        expect(items[0].sell_in).to eq 10
+      end
     end
   
   end
