b04318f8f8a8f92b45defc19234894fcd9a3a4ef
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-17 11:05:09 -0500

    Add test for backstage pass sell_in from 6 to 10

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -133,6 +133,28 @@ describe GildedRose do
         expect(items[1].quality).to eq 50
       end
 
+      it "increases the quality of 1 if the sell_in date is greater than 10" do
+        item_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 0)
+        item_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 30, 0)
+        items = [item_1, item_2]
+
+        GildedRose.new(items).update_quality()
+        
+        expect(item_1.quality).to eq 1
+        expect(item_2.quality).to eq 1
+      end
+
+      it "increases the quality by 2 if the sell_in date is from 6 to 10" do
+        item_1 = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+        item_2 = Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 0)
+        items = [item_1, item_2]
+
+        GildedRose.new(items).update_quality()
+        
+        expect(item_1.quality).to eq 2
+        expect(item_2.quality).to eq 2
+      end
+
     end
 
     context "Sulfuras" do
