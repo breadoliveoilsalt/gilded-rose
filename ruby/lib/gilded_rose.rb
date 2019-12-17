68103627b5066c681edb29325a272da16acf9ee0
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-12-17 14:49:10 -0500

    Isolate logic for Aged Brie rules in a conditional

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,7 +9,19 @@ class GildedRose
       if item.name == "Sulfuras, Hand of Ragnaros"
         next
       end
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+
+
+      if item.name == "Aged Brie"
+        if item.sell_in > 0
+          item.quality = [50, item.quality + 1].min
+        else
+          item.quality = [50, item.quality + 2].min
+        end
+        item.sell_in = item.sell_in - 1
+        next
+      end
+
+      if item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           item.quality = item.quality - 1
         end
@@ -34,18 +46,12 @@ class GildedRose
       item.sell_in = item.sell_in - 1
 
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality - item.quality
         end
       end
     end
